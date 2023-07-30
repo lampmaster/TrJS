9a68b621803e13f1e4f3fb9b216e008c695f9d51
“Nik” <playpaper12gmail.com>
2023-07-30 18:27:13 +0300

    init project

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  moduleFileExtensions: [
+    "js",
+    "mjs",
+    "cjs",
+    "ts",
+    "json",
+    "node"
+  ],
+  roots: [
+    "<rootDir>/src"
+  ],
+};
